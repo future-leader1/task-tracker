6f97c15d624f68d1ae69f8dc75f4426c49baf1f8
future-leader1 <mr.alijonov.azizbek@gmail.com>
2023-01-04 22:25:15 +0900

    delete,toggle,add,form implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,73 @@
 import Header from "./componenet/Header";
+import Tasks from "./componenet/Tasks";
+import { useState } from "react";
+import AddTask from "./componenet/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Study",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Meet Lawyer",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+  ]);
+
+  //toggle reminder
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
+  //add Task
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  //delete Task
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="container">
-      <Header />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks
+          tasks={tasks}
+          onDelete={deleteTask}
+          onToggle={toggleReminder}
+          onAdd={addTask}
+        />
+      ) : (
+        "NO Task TO Do"
+      )}
     </div>
   );
 }
